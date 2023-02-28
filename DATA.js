97dd6d486f1fb3e9b36422fbb09be09d926705b1
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-28 12:30:25 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "3734a254-456b-48dd-969f-dd410cee3b26",
+    "home_feed": [
+        {
+            "id": "d8df77fd-3ae2-4cf0-b26d-b6c3a062ca73",
+            "artist_name": "benefit",
+            "home_carousel": [
+                {
+                    "id": "cf5add71-2d6f-4973-bfbb-4858f9a6d05d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/48.png"
+                },
+                {
+                    "id": "b9da2a3b-4c9d-42c5-a6ce-f56d52d3ac32",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/79.png"
+                },
+                {
+                    "id": "a90da93e-17cd-40c5-b85a-4331e1dec10e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/48.png"
+                },
+                {
+                    "id": "804c2644-e146-40b5-a128-60c223f11a16",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/286.png"
+                },
+                {
+                    "id": "d875b657-d4cb-498a-aa74-ff5b0d4c6728",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/99.png"
+                }
+            ],
+            "art_caption": "until",
+            "art_description": "Notice decade TV agent late standard table. Away statement toward model good."
+        },
+        {
+            "id": "400a7a43-c8ee-4829-9338-501f1ece1dfc",
+            "artist_name": "individual",
+            "home_carousel": [
+                {
+                    "id": "ccf24017-bfd6-4515-961e-a24b4c290b9e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/183.png"
+                },
+                {
+                    "id": "0c269a80-94bd-45a9-b2e1-f11b0940b517",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/183.png"
+                },
+                {
+                    "id": "efc24906-863a-4e4d-81d7-a2758f59ce6e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/313.png"
+                },
+                {
+                    "id": "94960e2d-a5b2-4fde-a063-544468da7ec2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/252.png"
+                },
+                {
+                    "id": "f37dc99b-dd14-4826-bf2e-4d33e5ed6269",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/331.png"
+                }
+            ],
+            "art_caption": "charge",
+            "art_description": "Senior year middle type able itself however. That player anyone. Low drop talk. Adult dinner fill who without individual."
+        },
+        {
+            "id": "dd5e7777-5338-43df-b032-eda13a0af7d1",
+            "artist_name": "oil",
+            "home_carousel": [
+                {
+                    "id": "fea2c6b9-40c5-4c44-9f8a-d9d392a93a98",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/210.png"
+                },
+                {
+                    "id": "d2edbe01-50fd-4668-9f2f-0a604913c22d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/115.png"
+                },
+                {
+                    "id": "9f8e4de9-021a-4db7-8da5-06dfc5c3ca2b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/235.png"
+                },
+                {
+                    "id": "b61fc095-444c-4093-bcfd-c4b92599c424",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/236.png"
+                },
+                {
+                    "id": "4d158f69-0c1d-4798-b5fd-d35e4c00b103",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/265.png"
+                }
+            ],
+            "art_caption": "everybody",
+            "art_description": "Actually position class PM worry company kind change. Put old radio television treat. Audience raise any even leader church director."
+        },
+        {
+            "id": "0a66be48-6b1b-4da1-9325-0b520a43ab83",
+            "artist_name": "least",
+            "home_carousel": [
+                {
+                    "id": "a73fe3dd-554f-4e52-b018-a7734ef6951a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/155.png"
+                },
+                {
+                    "id": "d97ab80b-f4bb-4b19-91d4-f0105426bc60",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/318.png"
+                },
+                {
+                    "id": "0ccb7256-8265-49aa-b6da-7dc1643f7c47",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/334.png"
+                },
+                {
+                    "id": "f7c988dd-da21-4086-8259-5a9ab9c6bcae",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png"
+                },
+                {
+                    "id": "ef81799e-20df-4230-90fc-949353d88be5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/200.png"
+                }
+            ],
+            "art_caption": "trade",
+            "art_description": "Enough oil run page. Traditional tend short fish may again. Film scientist he night plant."
+        },
+        {
+            "id": "f604b4b7-2a22-4c8d-8da9-252d44de40b0",
+            "artist_name": "young",
+            "home_carousel": [
+                {
+                    "id": "1b63e87e-ccc0-43b1-b91a-cdf53d8a9e35",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/342.png"
+                },
+                {
+                    "id": "9d09fe86-0f73-401c-a0de-15bb6501e9a5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/165.png"
+                },
+                {
+                    "id": "c7c6e349-cbfe-4d34-8ba4-13dcddfe917f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/4.png"
+                },
+                {
+                    "id": "666e329a-5632-430d-8b86-1319ca8980e3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/47.png"
+                },
+                {
+                    "id": "19e412d8-5924-4bc3-a369-75ceea9a7d8b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/74.png"
+                }
+            ],
+            "art_caption": "least",
+            "art_description": "That dinner such enjoy day. Increase source finally dark. Five option assume follow bag data."
+        }
+    ]
+}
